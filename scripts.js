d520fc73e75afe898ea3370c3cdeb39c92b41f07
Radmaryb <Radmaryb@gmail.com>
2021-03-15 17:57:03 -0400

    first commit to task2

--- scripts.js
+++ scripts.js
@@ -1,19 +1,39 @@
 
-let firstItem = prompt(`Enter the first grocery item:`);
-var amount1 = prompt(`How many ${firstItem} would you like?`);
+//let firstItem = prompt(`Enter the first grocery item:`);
+//var amount1 = prompt(`How many ${firstItem} would you like?`);
 
-let secondItem = prompt(`Enter the second grocery item:`);
-var amount2 = prompt(`How many ${secondItem} would you like?`);
+//let secondItem = prompt(`Enter the second grocery item:`);
+//var amount2 = prompt(`How many ${secondItem} would you like?`);
 
-let thirdItem = prompt(`Enter the third grocery item:`);
-var amount3 = prompt(`How many ${thirdItem} would you like?`);
+//let thirdItem = prompt(`Enter the third grocery item:`);
+//var amount3 = prompt(`How many ${thirdItem} would you like?`);
 
 
-console.log(`Shopping List
-${amount1} ${firstItem}
-${amount2} ${secondItem} 
-${amount3} ${thirdItem}`);
+//console.log(`Shopping List
+//${amount1} ${firstItem}
+//${amount2} ${secondItem} 
+//${amount3} ${thirdItem}`);
 
-let total = (Number(amount1) + Number(amount2) + Number(amount3));
-console.log(`Total number of items ${total}`);
+//let total = (Number(amount1) + Number(amount2) + Number(amount3));
+//console.log(`Total number of items ${total}`);
+
+
+//Task 2
+
+let tongueTwister = "How much wood would a woodchuck chuck, if the woodchuck could chuck wood?";
+
+console.log(tongueTwister.toLowerCase());
+//Output:
+//"how much wood would a woodchuck chuck, if the woodchuck could chuck wood?"
+
+let pangram = "The quick brown fox jumps over the lazy dog.";
+console.log(pangram.indexOf("fox"));
+//Output:
+//16
+
+let anotherPangram = "The five boxing wizards jump quickly.";
+console.log(anotherPangram.charAt(11));
+//Output:
+//x
+
 
